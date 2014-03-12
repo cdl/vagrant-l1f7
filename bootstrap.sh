89be9cd2b2820b9a6bcb6b89307363ed5943b37c
Colby Ludwig <me@colbyludwig.com>
2014-03-12 09:45:56 -0600

    add pip to bootstrap script, add postgres dependencies, silence output

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,10 +7,23 @@ yellow=$(tput setaf 2)
 
 # general package upgrades
 echo "[provision] General distribution-based package upgrades."
-# apt-get update -y
-# apt-get upgrade -y
+apt-get update -y >/dev/null
+
+echo "[provision] Installing Python, pip, and Postgres dependencies."
+apt-get install python-pip -y >/dev/null
+sudo apt-get build-dep python-psycopg2 -y >/dev/null
 
 # regen locales
 echo "[provision] Generate Canadian locale for en_CA terminal setups."
-locale-gen en_CA.UTF-8
+locale-gen en_CA.UTF-8 >/dev/null
 echo LC_ALL=\"en_CA.UTF-8\" >> /etc/environment
+
+# pip install -r app/requirements.txt if it exists
+FILE=/home/vagrant/app/requirements/local.txt
+if [ -f $FILE ]
+then
+  echo "[provision] Running pip install."
+  pip install -r $FILE
+else
+  echo "[warning] No app/requirements.txt found, could not run pip install."
+fi

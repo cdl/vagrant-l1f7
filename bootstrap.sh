eddd966f5b069f8bf6fe30d562095fcda5355574
Colby Ludwig <me@colbyludwig.com>
2014-03-12 10:27:57 -0600

    add Django environment setup (exports, etc)

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+## Project-specific variables
+## Be sure to change these to match the project you're working on!
+export PROJECTNAME='project' # <= Django folder name
+
+
+
+export DJANGO_SETTINGS_MODULE=$PROJECTNAME.settings.dev
+export OLD_PYTHONPATH="$PYTHONPATH"
+export PYTHONPATH=$PYTHONPATH:/home/vagrant/app/$PROJECTNAME/
+
+
+
 # Variables for colour highlighting
 textreset=$(tput sgr0)
 red=$(tput setaf 1)
@@ -9,9 +21,16 @@ yellow=$(tput setaf 2)
 echo "[provision] General distribution-based package upgrades."
 apt-get update -y >/dev/null
 
-echo "[provision] Installing Python, pip, and Postgres dependencies."
+echo "[provision] Installing Python, pip, and PIL dependencies."
 apt-get install python-pip -y >/dev/null
-sudo apt-get build-dep python-psycopg2 -y >/dev/null
+apt-get build-dep python-psycopg2 -y >/dev/null
+apt-get install git -y >/dev/null
+
+# PIL dependencies
+apt-get install libjpeg8 libpng12-0 libfreetype6 zlib1g -y >/dev/null
+ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
+ln -s /usr/lib/x86_64-linux-gnu/libfreetype.so /usr/lib
+ln -s /usr/lib/x86_64-linux-gnu/libz.so /usr/lib
 
 # regen locales
 echo "[provision] Generate Canadian locale for en_CA terminal setups."
